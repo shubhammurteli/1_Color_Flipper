6a7d907d8b42ec81001020cef301660a1e598621
Shubham Murteli <49732743+shubhammurteli@users.noreply.github.com>
2020-07-20 21:47:27 +0530

    Added Responsivness

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # Introduction
 This is a JS Project which changes the background on click
-# Have a Look
-https://shubhammurteli.github.io/1_Color_Flipper/
--- hexapp.js
+++ hexapp.js
@@ -2,7 +2,7 @@ const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const colorName = document.getElementById("color");
 
-btn.addEventListener("mouseover", changeColor);
+btn.addEventListener("click", changeColor);
 
 function changeColor(){
     let color = "#";
